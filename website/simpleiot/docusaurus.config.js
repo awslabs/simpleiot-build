deb030f3f7d418bb1684c0ec2d0f7495db0600a4
Ramin Firoozye <framin@amazon.com>
2022-07-24 15:45:06 -0700

    Fixed baseUrl in config.js

--- website/simpleiot/docusaurus.config.js
+++ website/simpleiot/docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'SimpleIOT',
   tagline: 'Connected devices made easy.',
-  url: 'https://awslabs.github.io/simpleiot-build',
-  baseUrl: '/',
+  url: 'https://awslabs.github.io/simpleiot-build/',
+  baseUrl: '/simpleiot-build/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.png',
