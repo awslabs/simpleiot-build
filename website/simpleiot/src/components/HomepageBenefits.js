bb17b61d1d8a364bbc4d6e3f34ed8b1bcb5cc8b2
Ramin Firoozye <framin@amazon.com>
2022-07-25 10:40:44 -0700

    Minor homepage edits

--- website/simpleiot/src/components/HomepageBenefits.js
+++ website/simpleiot/src/components/HomepageBenefits.js
@@ -13,15 +13,15 @@ export default function HomepageBenefits() {
 
                         <dl>
                             <dt>Accelerate time to market</dt>
-                            <dd>SimpleIoT does the undifferentiated heavy lifting for you, all in one click.</dd>
+                            <dd>SimpleIOT does the undifferentiated heavy lifting for you so you can focus on your application.</dd>
                             <dt>Own your own data</dt>
-                            <dd>With SimpleIoT, you host your own infrastructure and data. Stop paying to access your own data!</dd>
-                            <dt>End to end security</dt>
-                            <dd>SimpleIoT is designed with AWS security best practices end-to-end.</dd>
+                            <dd>With SimpleIOT, you host your own infrastructure, code, and data.</dd>
+                            <dt>End-to-end security</dt>
+                            <dd>Designed with AWS security best-practices.</dd>
                             <dt>Scalable</dt>
-                            <dd>SimpleIoT scales seamlessly from one device to millions, no additional configuration needed.</dd>
+                            <dd>Scale seamlessly from one device to millions, with no additional configuration needed.</dd>
                             <dt>Pay-as-you-go</dt>
-                            <dd>SimpleIoT leverages the latest AWS Serverless and IoT services - only pay for what you use!</dd>
+                            <dd>SimpleIOT leverages the latest AWS Serverless and IOT services - only pay for what you use!</dd>
                         </dl>
                     </div>
                 </div>
