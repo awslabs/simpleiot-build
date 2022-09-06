8eed3ea709e196b1bc490b95eebbd9bd59c453cb
Ramin Firoozye <framin@amazon.com>
2022-09-06 15:55:51 -0700

    Added directl link to Starter kit from M5Stack

--- website/simpleiot/src/components/HomePageStarterKit.js
+++ website/simpleiot/src/components/HomePageStarterKit.js
@@ -10,7 +10,7 @@ export default function HomepageStarterKit() {
             <div className="container">
                 <div className="row">
                     <div className="col col--6">
-                        <h2>SimpleIOT Starter Bundle (coming soon)</h2>
+                        <h2>SimpleIOT Starter Bundle</h2>
                         <div>
                             <div id="starter-image">
                                 <img width="60%" src={useBaseUrl('/img/StarterBundle-med.png')} />
@@ -22,7 +22,7 @@ export default function HomepageStarterKit() {
                         <br />
                         <ul>
                             <li>All-in-one package. Get started experimenting with a fully-built and connected device in less than 5 minutes.</li>
-                            <li>Includes an ESP32-based processor, wiring, and multiple sensors. Individual items can be <a href="/docs/workshops/intro">purchased here.</a></li>
+                            <li>Includes an ESP32-based processor, wiring, and multiple sensors. <a href="https://shop.m5stack.com/products/aws-iot-edukit-simpleiot-starter-bundle-v1-0" target="_blank">Purchase a starter kit here.</a> Individual items can also be <a href="/docs/workshops/intro">purchased here.</a></li>
                             <ul>
                                 <li>Button press</li>
                                 <li>Color LED</li>
